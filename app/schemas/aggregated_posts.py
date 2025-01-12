6c2169903ddb82a040e63f88cbe693c16c7a8afa
Pavel Kochkin <kochkin27@gmail.com>
2025-01-12 18:45:41 +0300

    Add QuestionRequest model to handle user queries and cluster information

--- app/schemas/aggregated_posts.py
+++ app/schemas/aggregated_posts.py
@@ -13,3 +13,10 @@ class AggregatedPostModel(BaseModel):
     cluster: int
     title: str
     posts: List[PostModel]
+
+
+class QuestionRequest(BaseModel):
+    user_id: str
+    clusters: List[int]
+    digest_text: str
+    query_history: List[str]
